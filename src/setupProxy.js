e2a0defc0edb105bf7b217626763460e680bb392
rahulmeher013 <rahulmeher013@gmail.com>
2021-04-29 01:15:10 +0530

    add redux to filter component

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,12 +1,7 @@
-// const { createProxyMiddleware } = require('http-proxy-middleware');
-
-// module.exports = function(app) {
-//   app.use(
-//     '/api',
-//     createProxyMiddleware({
-//       target: 'http://127.0.0.1:5000/',
-//       changeOrigin: true,
-//     //   secure: false
-//     })
-//   );
-// };
+const proxy = require("http-proxy-middleware");
+module.exports = function(app) {
+  app.use(proxy("/api/**", { // https://github.com/chimurai/http-proxy-middleware
+    target: "http://127.0.0.1:5000",
+    secure: false
+  }));
+};
